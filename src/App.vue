d37ae7240b1958dcceb5d87a0b4b4416259f6900
周世林 <2844420026@qq.com>
2022-06-26 09:45:22 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+   123
+</div>
+</template>
+
+<script>
+export default {
+  created () {},
+  data () {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  filters: {},
+  components: {}
+}
+</script>
+
+<style scoped lang='less'>
+
+</style>
